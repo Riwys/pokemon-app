2e8b674d476ae982104adeb2cf474b9f06255f77
Riwys <cryitiy@gmail.com>
2024-12-11 18:10:19 +0530

    Simplifying code for better readability

--- src/components/pokemon/pokemon.js
+++ src/components/pokemon/pokemon.js
@@ -2,10 +2,10 @@ function Pokemon({pokemonData, pokemonReceived}) {
     if (pokemonReceived) {
         return (
             <div className="w-40 shadow-2 center ph1 pv2 mv4 bg-red">
-             <h2>{pokemonData.data.name}</h2>
-             <h3>{`experience: ${pokemonData.data.base_experience}`}</h3>
+             <h2>{pokemonData.name}</h2>
+             <h3>{`experience: ${pokemonData.base_experience}`}</h3>
              <img 
-             src={pokemonData.data.sprites.front_default} 
+             src={pokemonData.sprites.front_default} 
              alt='Pokemon' 
              />  
            </div>
